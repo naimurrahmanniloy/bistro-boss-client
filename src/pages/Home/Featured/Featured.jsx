26fa1b4a96a0912b9d78c784fdae9b9fac9c4e57
naimurrahmanniloy <niloyrahman432@gmail.com>
2023-09-04 10:47:02 +0600

    1st module done

--- src/pages/Home/Featured/Featured.jsx
+++ src/pages/Home/Featured/Featured.jsx
@@ -4,9 +4,9 @@ import featuredImg from '../../../assets/home/featured.jpg'
 
 const Featured = () => {
     return (
-        <div className="featured-item text-white pt-10 my-20">
+        <div className="featured-item bg-fixed text-white pt-10 my-20">
             <SectionTitle subHeading="check it out" heading="Featured Item"></SectionTitle>
-            <div className="md:flex justify-center items-center pb-20 pt-12 px-36">
+            <div className="md:flex justify-center items-center bg-slate-500 bg-opacity-40 pb-20 pt-12 px-36">
                 <div>
                     <img src={featuredImg} alt="" />
                 </div>
@@ -14,7 +14,7 @@ const Featured = () => {
                     <p>Aug 20, 2029</p>
                     <p className="uppercase">WHERE CAN I GET SOME?</p>
                     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste sit impedit quasi, doloremque natus recusandae quas assumenda quod doloribus accusamus quo culpa id porro atque ducimus saepe excepturi veritatis. Consequuntur!</p>
-                    <button className="btn btn-outline">Order Now</button>
+                    <button className="btn btn-outline border-0 border-b-4 mt-5">Order Now</button>
                 </div>
             </div>
         </div>
